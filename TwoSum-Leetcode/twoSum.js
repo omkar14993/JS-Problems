02900df138bb5712a464f9593abb60888b46d622
omkar14993 <omkarsalunke14993@gmail.com>
2021-04-09 10:19:02 -0400

    twoSum-leetCode

---
+++ TwoSum-Leetcode/twoSum.js
@@ -0,0 +1,24 @@
+let nums = [2, 5, 6, 11, 15]
+let target = 8;
+
+// Brute force method, o(n^2)
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let first = nums[i];
+        for (let j = 0; j < nums.length; j++) {
+            if (target - nums[i] === nums[j])
+                return [i, j];
+        }
+    }
+}
+
+// using HashMap in JS, o(n)
+function twoSumMap(nums, target) {
+    let mapSum = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (mapSum.has(target - nums[i])) {
+            return [mapSum.get(target - nums[i]), i]
+        }
+        mapSum.set(nums[i], i)
+    }
+}
